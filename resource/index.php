479a1abc8e6bbf20f63bd69180210d28a842cf25
ryan(김남태) <ryan@mibank.me>
2021-04-26 15:21:05 +0900

    초기파일로만 구성(2)

--- resource/index.php
+++ resource/index.php
@@ -8,13 +8,7 @@ if (isset($_COOKIE['SAVE_ID'])) {
 }
 ?>
 <div class="login-box">
-	<h1>배송관리자</h1>
-	<p class="copy">마이뱅크 공항외화배송 서비스</p>
-	<form action="" method="post" name="LoginForm">
-		<input type="text" class="email" name="userEmail" placeholder="이메일" value="<?=$saveID?>">
-		<input type="password" class="password" name="userPwd" placeholder="비밀번호">
-		<button type="button" onclick="document.LoginForm.submit()">로그인</button>
-		<div class="label-wrap"><input type="checkbox" id="email-save" name="emailSave" <?=$checkSave === true ? 'checked' : ''?>><label for="email-save">이메일 저장</label></div>
-	</form>
+	<h1>INDEX</h1>
+	<p class="copy">MAIN</p>
 </div>
 <?php include_once 'include/common.footer.php'; ?>
